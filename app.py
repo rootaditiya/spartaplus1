116a69f09ae5a7c27c2f2e1fa7fa39bf3aca6c50
rootaditiya <lifehealt016@gmail.com>
2023-11-20 02:19:33 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,60 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from datetime import datetime
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+	articles = list(db.diary.find({},{'_id':False}))
+	return jsonify({'articles': articles})
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+	postime = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+
+	title_receive = request.form.get('title_give')
+	content_receive = request.form.get('content_give')
+
+	file = request.files['file_give']
+	extension = file.filename.split('.')[-1]
+	filename = f'static/file_{postime}.{extension}'
+	file.save(filename)
+
+	profile = request.files['profile_give']
+	extension = profile.filename.split('.')[-1]
+	profilename = f'static/user_{postime}.{extension}'
+	profile.save(profilename)
+
+	doc = {
+		'file': filename,
+		'profile': profilename,
+		'title': title_receive,
+		'content': content_receive,
+		'time': datetime.now().strftime('%Y.%m.%d'),
+	}
+	db.diary.insert_one(doc)
+	return jsonify({
+		 'message':'Data was saved'
+		})
+
+if __name__ == '__main__':
+	app.run('0.0.0.0', port=5000, debug=True)
